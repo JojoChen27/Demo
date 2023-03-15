c09d549046be7da1d6afc12a155c9461f3d9e39c
陈超 <cchao2627@163.com>
2023-03-15 19:24:43 +0800

    添加测试代码

--- GithubActionsDemo/GithubActionsDemo/ViewController.swift
+++ GithubActionsDemo/GithubActionsDemo/ViewController.swift
@@ -7,11 +7,25 @@
 
 import UIKit
 
+extension String {
+    func localized() -> String {
+        if let path = Bundle.main.path(forResource: "en", ofType: "lproj"),
+           let bundle = Bundle(path: path) {
+            return bundle.localizedString(forKey: self, value: nil, table: "Localizable")
+        }
+        return self
+    }
+
+    func localizedFormat(_ arguments: CVarArg...) -> String {
+        String(format: localized(), arguments: arguments)
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "测试文本"
     }
 
 
