1ad76167fbb8a33d9fbf30cdb50ad0c5407b1a47
陈超 <cchao2627@163.com>
2023-03-16 17:03:31 +0800

    Update ViewController.swift

--- GithubActionsDemo/GithubActionsDemo/ViewController.swift
+++ GithubActionsDemo/GithubActionsDemo/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
         title = "测试".localized()
         title = "测试1".localized()
         title = "测试3".localized()
-        title = "测试4"
+        title = "测试4" 
     }
 
 
